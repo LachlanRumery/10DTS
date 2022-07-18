d552c480343f76f3242b61a1fd9c1e1f67ae0725
Lachlan Rumery <99456821+LachlanRumery@users.noreply.github.com>
2022-07-18 13:59:06 +1000

    👨‍💻18/07/22 -> DTS
    Python copy of pseudocode answers from input, process, output worksheet.

---
+++ Python/IPO Worksheet.py
@@ -0,0 +1,14 @@
+def PRODUCT_TWO_NUMS(n1,n2):
+    return n1*n2
+
+def SQUARE(n):
+    return n*n
+
+def AREA_RIGHT_ANGLED_TRIANGLE(base,height):
+    return 0.5*base*height
+
+def DAYS_TO_COVER(KMperDAY,KM):
+    return KM/KMperDAY
+
+def STUDENT_APPLE_BASKET(students,apples):
+    return(apples % students,(apples-(apples % students))/students)
