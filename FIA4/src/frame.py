33bd467385c2f3208fdc2bf4c2e883385e98a0ca
Lachlan Rumery <99456821+LachlanRumery@users.noreply.github.com>
2022-11-09 23:38:17 +1000

    25/10 - Final Submission - Finished :)

--- FIA4/src/frame.py
+++ FIA4/src/frame.py
@@ -1,14 +1,33 @@
+﻿#  MIT License
+#
+#  Copyright (c) 2022 Lachlan Rumery
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
 import tkinter as tk
 
 import matplotlib
 import matplotlib.ticker as mticker
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # , NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 from data_reader import DataHandler
-
-# matplotlib import statements
-
 
 matplotlib.use('TKAgg')
 
@@ -17,10 +36,18 @@ H1 = ('Monospace', 20)
 
 # Global
 global variable
+global canvas
+
+# Graph
+figure = Figure(figsize=(10, 5), dpi=100)
+axis = figure.add_subplot(111)
 
 
 class QLDCrimeReport(tk.Tk):
-	def __init__(self, *args, **kwargs):
+	def __init__(self, *args, **kwargs) -> None:
+		"""
+		Initialises TKinter frame
+		"""
 		tk.Tk.__init__(self, *args, **kwargs)
 		window = tk.Frame(self)
 
@@ -37,20 +64,21 @@ class QLDCrimeReport(tk.Tk):
 			frame.grid(row=0, column=0, sticky='nsew')
 		self.display_window(LandingPage)
 
-	def display_window(self, window):
+	def display_window(self, window) -> None:
 		"""
         Display window
 
-        :param window:
-        :return:
+        :param window: Window to open
         """
+		if window == LandingPage:
+			graph_control(0)
 		window = self.windows[window]
 		window.config(bg='white')
 		window.tkraise()
 
 
 class LandingPage(tk.Frame):
-	def __init__(self, parent, controller):
+	def __init__(self, parent, controller) -> None:
 		tk.Frame.__init__(self, parent)
 		label = tk.Label(self, text="Queensland Crime Report", font=H1)
 		label.config(bg='white')
@@ -77,21 +105,25 @@ class GraphPage(tk.Frame):
 		button = tk.Button(self, text='Return', command=lambda: controller.display_window(LandingPage))
 		button.pack()
 
-		crime = variable.get()
-		crimeData = DataHandler('../resources/Raw.csv').month_totals(crime)
+		global canvas
+		canvas = FigureCanvasTkAgg(figure, self)
 
-		f = Figure(figsize=(10, 5), dpi=100)
-		f.suptitle(crime)
-		a = f.add_subplot(111)
-		a.plot(crimeData.keys(), crimeData.values(), label=crime)
-		f.legend()
-		mylocator = mticker.MultipleLocator(12)
-		a.xaxis.set_major_locator(mylocator)
 
-		f.autofmt_xdate()
+def graph_control(i) -> None:
+	print(variable.get())
 
-		canvas = FigureCanvasTkAgg(f, self)
-		canvas.get_tk_widget().pack()
+	crime = variable.get()
+	crimeData = DataHandler('../resources/Raw.csv').month_totals(crime)
+
+	axis.plot(crimeData.keys(), crimeData.values(), label=crime)
+	figure.suptitle(crime)
+	figure.legend()
+
+	axis.xaxis.set_major_locator(mticker.MultipleLocator(12))
+	figure.autofmt_xdate()
+
+	canvas.get_tk_widget().pack_forget()
+	canvas.get_tk_widget().pack()
 
 
 App = QLDCrimeReport()
